b44b410dee52ae32d0a6f7d6b104827890b6c430
huicong2002 <1532751400@qq.com>
2021-03-19 21:31:47 +0800

    logs

---
+++ 大一算法题20210315A/大一算法题20210315A.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int arr[1010];
+int main()
+{
+	int N, n, m;
+	int i;
+	int sum1, sum2, time;
+
+	scanf("%d", &N);
+
+	while (N--)
+	{
+		scanf("%d", &n);
+
+		for (i = 1; i <= n; i++)
+		{
+			scanf("%d", &arr[i]);
+\
+		}
+
+		time = 0;
+
+		while (n)
+		{
+			if (n >= 4)
+			{
+				sum1 = arr[n] + arr[1] + arr[n - 1] + arr[1];
+				sum2 = arr[2] + arr[1] + arr[n] + arr[2];
+				time += (sum1 > sum2 ? sum2 : sum1);
+				n -= 2;
+			}
+			else if (n == 3)
+			{
+				sum1 = arr[n] + arr[1] + arr[n-1];
+				time = time + sum1;
+				break;
+			}
+			else
+			{
+				time += arr[n];
+				break;
+			}
+		}
+
+		printf("%d\n", time);
+	}
+
+	return 0;
+}
